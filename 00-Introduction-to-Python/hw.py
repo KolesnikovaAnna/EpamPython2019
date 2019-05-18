d314c3e00e5fafba91810f878dae5474f4ad9ef1
KolesnikovaAnna <31996544+KolesnikovaAnna@users.noreply.github.com>
2019-05-18 21:24:53 +0300

    Add files via upload

--- 00-Introduction-to-Python/hw.py
+++ 00-Introduction-to-Python/hw.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 """
 Реализуйте метод, определяющий, является ли одна строка 
 перестановкой другой. Под перестановкой понимаем любое 
@@ -9,7 +7,15 @@
 
 def is_permutation(a: str, b: str) -> bool:
     # Нужно проверить, являются ли строчки 'a' и 'b' перестановками
-
+	a = ''.join(sorted(a))
+	b = ''.join(sorted(b))
+	print(a, b)
+	if a == b:
+		print("T")
+		return True
+	else:
+		print("F")
+		return False
 
 assert is_permutation('baba', 'abab')
 assert is_permutation('abbba', 'abab')
